309f3d6eda3e0ae6d668ae3b7f9e8381e2926fe0
serhatyavuzyiit <soyavuzyigit@gmail.com>
2020-06-14 19:02:41 +0300

    project euler updated

---
+++ project euler/largest_prime_factor.py
@@ -0,0 +1,13 @@
+n = 600851475143
+factors = []
+d = 2
+while n > 1:
+    while n % d == 0:
+        factors.append(d)
+        n /= d
+    d = d + 1
+    if d * d > n:
+        if n > 1: factors.append(n)
+        break
+
+print(max(factors))
