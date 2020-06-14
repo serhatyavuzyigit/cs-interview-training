8d8d84b12efeaf04521f3af8bad868b66ba28923
serhatyavuzyiit <soyavuzyigit@gmail.com>
2020-06-14 19:11:37 +0300

    car_and_cdr.py added

---
+++ daily coding problems/car_and_cdr.py
@@ -0,0 +1,19 @@
+# This problem was asked by Jane Street.
+
+# cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element 
+# of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(cons):
+    return cons(lambda a, b: a)
+
+def cdr(cons):
+    return cons(lambda a, b: b)
+
+print(car(cons(3, 4)))
+print(cdr(cons(3, 4)))
