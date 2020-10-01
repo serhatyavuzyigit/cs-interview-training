e92fc1560c514a2bb0498c8fc7b488c022fcf5cc
serhatyavuzyiit <soyavuzyigit@gmail.com>
2020-10-01 20:51:49 +0300

    LongestCollatzSequence.java added

---
+++ project euler/LongestCollatzSequence.java
@@ -0,0 +1,29 @@
+package project_euler;
+
+public class LongestCollatzSequence {
+
+    public static void main(String[] args) {
+
+        long max = 0;
+        long res = 0;
+        for(long i=999999; i>700000; i--) {
+            long start = i;
+            long counter = 0;
+            while(start != 1) {
+                if(start%2 == 1)
+                    start = start*3 + 1;
+                else
+                    start = start / 2;
+                counter++;
+
+            }
+            if(max < counter) {
+                max = counter;
+                res = i;
+                System.out.println(res);
+            }
+        }
+        System.out.println("num: " + res + " sequence: " + max);
+    }
+    
+}
